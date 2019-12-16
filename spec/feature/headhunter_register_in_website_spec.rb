c96efe10e0a3dc4f77721ad2795b27776f3ac601
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2019-12-16 17:04:34 -0300

    Headhunter can register. Configuration for different devise views

---
+++ spec/feature/headhunter_register_in_website_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'Headhunter register' do
+  scenario 'sucessfully' do
+    visit root_path
+    click_on 'Sou um CodeHunter!'
+
+    fill_in 'Email', with: 'test@test.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+    click_on 'Sign up'
+
+    expect(page).to have_content('Bem vindo! Você realizou seu registro com sucesso.')
+    expect(page).to have_content('Sair')
+  end
+  scenario 'and cannot register if email already in database' do
+    Headhunter.create!(email: 'test@test.com', password: '123456')
+
+    visit root_path
+    click_on 'Sou um CodeHunter!'
+
+    fill_in 'Email', with: 'test@test.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+    click_on 'Sign up'
+
+    expect(page).to have_content('Email já está em uso')
+  end
+end
