6a2152ec4012afaeaf8561ab1d5a604907385c2f
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2020-01-24 16:26:07 -0300

    Implementing should_matchers gem and tests

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe Application do
-
+  describe 'associations' do
+    it { should belong_to(:job) }
+    it { should belong_to(:candidate) }
+    it { should have_many(:messages) }
+    it { should have_one(:feedback) }
+    it { should have_one(:proposal) }
+  end
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 describe Job do
+  describe 'associations' do
+    it {
+      should belong_to(:headhunter)
+      should have_many(:applications)
+      should have_many(:candidates).through(:applications)
+    }
+  end
 
+  describe 'validations' do
+    it {
+      should validate_presence_of(:title)
+      should validate_presence_of(:level)
+      should validate_presence_of(:number_of_vacancies)
+      should validate_presence_of(:salary)
+      should validate_presence_of(:description)
+      should validate_presence_of(:abilities)
+      should validate_presence_of(:deadline)
+      should validate_presence_of(:start_date)
+      should validate_presence_of(:location)
+      should validate_presence_of(:contract_type)
+    }
+  end
 end
