c1c7af43ffb245da1c682beff2c9cf4d6acba220
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2020-01-25 20:00:30 -0300

    Added gem shoulda matchers to test models associations

--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe Proposal do
-
+  describe 'associations' do
+    it {
+      should belong_to(:application)
+      should belong_to(:headhunter)
+      should belong_to(:candidate)
+    }
+  end
 end
